dac78ab7139d97633b39b0c54f7b6d61c4aef8da
Carlete27 <marquezcalerocarlos@gmail.com>
2025-06-08 18:12:14 +0200

    Delete Proyecto Final de Grado - Carlos Márqeuz Calero/public directory

--- Proyecto Final de Grado - Carlos Márqeuz Calero/public/views/footer.php
+++
@@ -1,7 +0,0 @@
-<!-- Pie de página fijo en la parte inferior con información de copyright -->
-<footer class="bg-primary text-white text-center py-3 mt-auto fixed-bottom">
-    &copy; <?= date("Y") ?> Carlete SL | Plataforma de Gestión de Usuarios
-</footer>
-
-</body>
-</html>
